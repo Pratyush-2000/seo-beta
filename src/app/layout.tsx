61c55b58b5fe3d2c60d736b60a03e7a720fb97e6
Pratyush-2000 <45617325+Pratyush-2000@users.noreply.github.com>
2024-01-30 18:08:02 +0530

    setting custom tag using window object

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
-import {useEffect} from "react";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,11 +16,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
-  useEffect(() => {
-    (window as any).clarity("set", "subdomain", "beta")
-  }, []);
-
 
   return (
     <html lang="en">
---
+++ src/app/test.tsx
@@ -0,0 +1,10 @@
+import {useEffect} from "react";
+
+const Test= ()=>{
+    useEffect(() => {
+        (window as any).clarity("set", "subdomain", "beta")
+    }, []);
+    return <></>
+}
+
+export default Test
